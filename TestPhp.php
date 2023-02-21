c7b689d4d59d81f5f9bc07fa919a05a95d878fbb
PhamTuanLinh <58585884+phamLinh98@users.noreply.github.com>
2023-02-21 22:43:57 +0900

    Linh Add Project
    test projects

---
+++ TestPhp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<?php
+echo 'Xin chao Tuan Linh';
+echo '<br>';
+echo 'Co nguoi yeu chua?';
+echo '<br>';
+echo 'Van e phai khong';
+echo 'test';
+echo 'test3';
+?>	
+</body>
+</html>
